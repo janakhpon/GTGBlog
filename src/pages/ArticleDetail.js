3d2a18ce4cb661eb52b11e542290377ed7f5b957
janakhpon <jnovaxer@gmail.com>
2020-02-14 22:07:20 +0630

    GDEV012 added styling options

--- src/pages/ArticleDetail.js
+++ src/pages/ArticleDetail.js
@@ -2,14 +2,30 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Chip from '@material-ui/core/Chip'
 
-const ArticleDetail = ({ location}) => (
-    <Layout>
-        <h1>LOL</h1>
+const ArticleDetail = ({ location }) => (
+  <Layout>
+    <h1>{location.state.frontmatter.title}</h1>
+    <h5>Published at {location.state.frontmatter.date}</h5>
+    <h5>Category : {location.state.frontmatter.category} </h5>
+    <h4>Author : {location.state.frontmatter.author} </h4>
+
+    <Typography paragraph style={{ fontSize: '0.80em', fontWeight: 'bold' }}>
+      {location.state.content.content}
+    </Typography>
+    <Grid container>
+      <Grid item xs={12}>
         {
-          console.log(location.state)
+          location.state.frontmatter.tags.map((tag, key) => {
+            return <Chip size="small" label={tag} key={key} />
+          })
         }
-    </Layout>
+      </Grid>
+    </Grid>
+
+  </Layout>
 )
 
 export default ArticleDetail
